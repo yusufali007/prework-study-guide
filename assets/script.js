7cbf00a6a1d88027723c3f3a1c294edc38fe3f13
unknown <unifiedexpert@gmail.com>
2023-11-03 21:08:16 +0000

    your message here

---
+++ assets/script.js
@@ -0,0 +1,30 @@
+
+var topics = ['HTML', 'CSS', 'GIT', 'JavaScript'];
+
+
+for (var x = 0; x < topics.length; x++){
+    console.log(topics[x]);
+
+}
+
+
+
+
+console.log(topics)
+
+
+/*
+if (topic == 'HTML'){
+console.log("lets study HTML");
+} else if (topic == 'CSS'){
+console.log("lets study CSS");
+} else if (topic == 'GIT'){
+console.log("lets study GIT");
+} else if (topic == JavaScript){
+    console.log(topic == 'JavaScript');
+} else {
+    console.log('please try again');
+}
+*/
+
+
