ffaf3f395565e2626fb2ec8047cc7b168ad414a0
unknown <unifiedexpert@gmail.com>
2023-11-05 20:10:53 +0000

    message for JS here

--- assets/script.js
+++ assets/script.js
@@ -1,30 +1,32 @@
 
-var topics = ['HTML', 'CSS', 'GIT', 'JavaScript'];
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
 
+function listTopics() {
+    for (var x = 0; x < topics.length; x++){
+        console.log(topics[x]);
 
-for (var x = 0; x < topics.length; x++){
-    console.log(topics[x]);
-
+    }
 }
 
 
+/* console.log(topics) */
+function selectTopic(){
+        if (randomTopic == 'HTML'){
+        console.log("lets study HTML");
+        } else if (randomTopic == 'CSS'){
+        console.log("lets study CSS");
+        } else if (randomTopic == 'GIT'){
+        console.log("lets study GIT");
+        } else if (randomTopic == JavaScript){
+            console.log(randomTopic == 'JavaScript');
+        } else {
+            console.log('please try again');
+    }
 
-
-console.log(topics)
-
-
-/*
-if (topic == 'HTML'){
-console.log("lets study HTML");
-} else if (topic == 'CSS'){
-console.log("lets study CSS");
-} else if (topic == 'GIT'){
-console.log("lets study GIT");
-} else if (topic == JavaScript){
-    console.log(topic == 'JavaScript');
-} else {
-    console.log('please try again');
 }
-*/
 
-
+console.log('heres some topics we learned through prework');
+listTopics();
+console.log('which topic would yopu like to study?');
+selectTopic();
